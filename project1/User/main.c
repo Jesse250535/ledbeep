07b6ab97fe7011a373555f423123024bf67550c9
Jesse250535 <jim985211gx@163.com>
2025-12-13 21:26:01 +0800

    新增ds1302时钟芯片，并且增加lcd1602显示
    Signed-off-by: Jesse250535 <jim985211gx@163.com>

--- project1/User/main.c
+++ project1/User/main.c
@@ -1,6 +1,7 @@
 #include "smg.h"
 #include "ired.h"
 #include "lcd1602.h"
+#include "ds1302.h"
 #include "public.h"
 
 
@@ -11,8 +12,7 @@ void main()
 
 	ired_init();//红外初始化
 	lcd1602_init();//LCD1602初始化
-	lcd1602_show_string(0,0,"Hello World!");//第一行显示
-	lcd1602_show_string(0,1,"0123456789");//第二行显示
+
 
 	while(1)
 	{
@@ -63,6 +63,9 @@ void main()
 			led8_off();
 			delay_10us(100000);
 		}
+
+
+
 
 
 	}
